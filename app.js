86395b75b9293783aa89ed23924316d85e11fe00
serhathizlisoy-ship-it <serhathizlisoy@gmail.com>
2026-05-18 21:44:58 +0300

    apı

--- app.js
+++ app.js
@@ -442,6 +442,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').catch(() => {});
-}
--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
